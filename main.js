481966e0552de8a1ea2ef34c2b0c77eada54c515
JacvaSipt <steamcow12@hotmail.com>
2023-06-15 17:13:29 +0100

    change function to ES6

--- main.js
+++ main.js
@@ -1,56 +1,55 @@
 //selectors
-const todoInput = document.querySelector('.todo-input');
-const todoButton = document.querySelector('.todo-button');
-const todoList = document.querySelector('.todo-list');
-
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
 
 //event listeners
 
-todoButton.addEventListener('click', addTodo);
-todoList.addEventListener('click', deleteCheck);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
 
 //functions
 
-function addTodo(e){
-    //prevent form submit
-    e.preventDefault();
-    //todo div
-    const todoDiv = document.createElement('div')
-    todoDiv.classList.add('todo');
-    //li
-    const newTodo = document.createElement('li')
-    newTodo.innerText = todoInput.value;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
-    //check btn
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fas fa-check"></i>';
-    completedButton.classList.add('completed-btn')
-    todoDiv.appendChild(completedButton);
-    //remove btn
-    const removeButton = document.createElement('button');
-    removeButton.innerHTML = '<i class="fas fa-trash"></i>';
-    removeButton.classList.add('remove-btn')
-    todoDiv.appendChild(removeButton);
-    //append to list
-    todoList.appendChild(todoDiv);
-    //clear input value
-    todoInput.value = "";
-};
+function addTodo(e) {
+  //prevent form submit
+  e.preventDefault();
+  //todo div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //check btn
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton);
+  //remove btn
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = '<i class="fas fa-trash"></i>';
+  removeButton.classList.add("remove-btn");
+  todoDiv.appendChild(removeButton);
+  //append to list
+  todoList.appendChild(todoDiv);
+  //clear input value
+  todoInput.value = "";
+}
 
-function deleteCheck(e){
-    const item = e.target;
-    //delete item
-    if(item.classList[0] === 'remove-btn') {
-        const todo = item.parentElement;
-        todo.classList.add('fall-animation')
-        todo.addEventListener('transitionend', function(){
-            todo.remove()
-        });
-    };
+function deleteCheck(e) {
+  const item = e.target;
+  //delete item
+  if (item.classList[0] === "remove-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall-animation");
+    todo.addEventListener("transitionend", () => {
+      todo.remove();
+    });
+  }
 
-    if(item.classList[0] === 'completed-btn') {
-        const todo = item.parentElement;
-        todo.classList.toggle('completed')
-    };
-};
+  if (item.classList[0] === "completed-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
